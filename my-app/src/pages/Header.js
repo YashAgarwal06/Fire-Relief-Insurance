d99d3f1ec2ffbe412738064c8d67ed335ffea2dd
sophzh <sophzh@ucla.edu>
2025-01-26 11:56:33 -0800

    forgot the header component

---
+++ my-app/src/pages/Header.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Modal, Box, Button, Typography, AppBar, Toolbar } from '@mui/material';
+import { Link } from 'react-router-dom'; // Import Link for navigation
+import '../CoverClear.css';
+import { defaultUrlTransform } from 'react-markdown';
+
+
+const Header = () => {
+    return (
+        <div className="header">
+        {/* Header Section */}
+        <AppBar position="static" sx={{ backgroundColor: '#1f4d61' }}>
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Link to="/home" style={{ color: '#fff', textDecoration: 'none' }}>
+                        CoverClear
+                    </Link>
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        </div>
+    )
+}
+
+export default Header;
